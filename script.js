da6c3faa3d17221caf7c5faa7bc9364c68bf01ff
shubham1515 <shubhsingh1515@gmail.com>
2025-03-07 12:18:22 +0530

    added scroll to top button and contact us button

---
+++ script.js
@@ -0,0 +1,22 @@
+// Get the button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Show the button when scrolling down
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// Scroll to the top when the button is clicked
+scrollToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
